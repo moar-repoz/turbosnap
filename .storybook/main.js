fef79387e818342652f21614ba6f8ef933e3f2f1
Steven Kitterman <steven.kitterman@gmail.com>
2026-06-10 15:37:14 -0700

    asdf

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,8 @@ const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    "@storybook/addon-docs"
   ],
   framework: {
     name: "@storybook/react-vite",
