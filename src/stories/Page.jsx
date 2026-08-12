90272ee246b72f9a88e1efeeefbcca9538b3f1a9
Steven Kitterman <steven.kitterman@gmail.com>
2026-08-12 12:48:13 -0700

    Slight change

--- src/stories/Page.jsx
+++ src/stories/Page.jsx
@@ -28,7 +28,7 @@ export const Page = () => {
         <p>
           Render pages with mock data. This makes it easy to build and review page states without
           needing to navigate to them in your app. Here are some handy patterns for managing page
-          data in Storybook:
+          data in Storybeook:
         </p>
         <ul>
           <li>
