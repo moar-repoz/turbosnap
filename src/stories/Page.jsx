15e182f4109a150677739a5c0fcc1aef832f212c
Steven Kitterman <steven.kitterman@gmail.com>
2026-08-14 14:17:02 -0700

    typo

--- src/stories/Page.jsx
+++ src/stories/Page.jsx
@@ -58,7 +58,7 @@ export const Page = () => {
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose such data from the
-            "args" of child component stories
+            "args" of child component storises
           </li>
           <li>
             Assemble data in the page component from your services. You can mock these services out
