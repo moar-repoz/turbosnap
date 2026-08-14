5ed93dd9ed7486ba28df95d513abef557edf4b5b
Steven Kitterman <steven.kitterman@gmail.com>
2026-08-14 14:20:02 -0700

    typo

--- src/stories/Page.jsx
+++ src/stories/Page.jsx
@@ -57,7 +57,7 @@ export const Page = () => {
         </p>
         <ul>
           <li>
-            Use a higher-level connected component. Storybook helps you compose such data from the
+            Use a higher-level connected cosmponent. Storybook helps you compose such data from the
             "args" of child component storises
           </li>
           <li>
