82627cd22cfc783ed8c0521551bf532fc05c96d7
Steven Kitterman <steven.kitterman@gmail.com>
2026-08-17 13:32:32 -0700

    t

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -21,7 +21,7 @@ export const Button = ({
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label}sssnailssssssssswss4
+      {label}sssnailssssssssswss4t
     </button>
   );
 };
