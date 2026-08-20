7687ce764e15b22bb5b61831ec9020f8b5a1fdd3
Steven Kitterman <steven.kitterman@gmail.com>
2026-08-20 11:02:50 -0700

    Moar letters

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -21,7 +21,7 @@ export const Button = ({
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label}sssnailsssssssssswss4stssrhsts
+      {label}sssnailsssssssssswss4stssrhstss
     </button>
   );
 };
