9848eea353bedea8c56ed57ff52ff597252defb1
Steven Kitterman <steven.kitterman@gmail.com>
2026-08-20 11:09:20 -0700

    A nother change

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -21,7 +21,7 @@ export const Button = ({
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label}sssnaisslsssssssssswss4stssrhstss
+      d{label}sssnaisslsssssssssswss4stssrhstss
     </button>
   );
 };
