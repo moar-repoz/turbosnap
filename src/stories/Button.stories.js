af8ba616d4f68f0adeb8e1d35965b9354964dfb3
Steven Kitterman <steven.kitterman@gmail.com>
2026-09-09 17:17:13 -0700

    new

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,20 +1,20 @@
-import { fn } from '@storybook/test';
+import { fn } from "@storybook/test";
 
-import { Button } from './Button';
+import { Button } from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
+    layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    backgroundColor: { control: "color" },
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
@@ -24,26 +24,51 @@ export default {
 export const Primary = {
   args: {
     primary: true,
-    label: 'Button',
+    label: "Button",
   },
 };
 
 export const Secondary = {
   args: {
-    label: 'Button',
+    label: "Button",
   },
 };
 
 export const Large = {
   args: {
-    size: 'large',
-    label: 'Button',
+    size: "large",
+    label: "Button",
   },
 };
 
 export const Small = {
   args: {
-    size: 'small',
-    label: 'Button',
+    size: "small",
+    label: "Button",
+  },
+};
+
+const HEIGHT = 200;
+// hi one more
+const TallStory = () => (
+  <div
+    style={{
+      width: 320,
+      height: HEIGHT,
+      background: "linear-gradient(#663399, #ff4785)",
+      color: "white",
+      fontFamily: "sans-serif",
+      padding: 16,
+      boxSizing: "border-box",
+    }}
+  >
+    {HEIGHT.toLocaleString()} CSS pixels tall
+  </div>
+);
+
+export const Tall = {
+  render: () => <TallStory />,
+  parameters: {
+    chromatic: { viewports: [320] },
   },
 };
